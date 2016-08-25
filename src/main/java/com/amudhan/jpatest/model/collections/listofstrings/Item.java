2343a2781a4d1a0d16d054fa9fea56f053fbaacd
Amudhan <aravamudhan.a@gmail.com>
2016-08-25 19:26:57 +0530

    ignore test-output report;listofstrings

--- src/main/java/com/amudhan/jpatest/model/collections/listofstrings/Item.java
+++ src/main/java/com/amudhan/jpatest/model/collections/listofstrings/Item.java
@@ -12,16 +12,16 @@ import javax.persistence.Table;
 
 import com.amudhan.jpatest.model.AbstractItem;
 /*Images are mapped using List.
- * When an image value is deleted, hibernate issues one delete and few update queries.
+ * When an image value is deleted, hibernate issues one delete and 2 update queries.
  * These two queries are fired to update the positions of other elements.
- * When an item is deleted in a list, two items before and after need to be updated.*/
+ * When an item is deleted in a list, two items before and after that need to be updated.*/
 @Entity(name="COLLECTIONS_LISTOFSTRINGS_ITEM")
 @Table(name="COLLECTIONS_LISTOFSTRINGS_ITEM")
 public class Item extends AbstractItem{
 
 	@ElementCollection
 	@CollectionTable(name="COLLECTIONS_LISTOFSTRING_IMAGE")
-	@OrderColumn//Stores an index in the persistent list starting at 0.
+	@OrderColumn(name="ITEM_ORDER")//Stores an index in the persistent list starting at 0.
 	@Column(name="FILE_NAME")
 	private List<String> images = new ArrayList<String>();
 	
---
+++ src/test/java/com/amudhan/jpatest/model/collections/listofstrings/ItemTest.java
@@ -0,0 +1,50 @@
+package com.amudhan.jpatest.model.collections.listofstrings;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.annotations.Test;
+
+import com.amudhan.jpatest.SuperItemTest;
+
+public class ItemTest extends SuperItemTest {
+	
+	@Test
+	@Transactional
+	public void getItems(){
+		Item item = new Item();
+		item.setItemName(RandomStringUtils.randomAlphabetic(10));
+		item.getImages().add(RandomStringUtils.randomAlphabetic(5));
+		entityManager.persist(item);
+		entityManager.flush();
+		Item persistedItem = entityManager.find(Item.class, item.getId());
+		logger.info("Item ID: "+persistedItem.getId()+" Item name:"+persistedItem.getItemName());
+		for(String image : persistedItem.getImages()){
+			logger.info("Image file name :"+image);
+		}
+	}
+	@Test
+	@Transactional
+	public void deleteImage(){
+		Item item = new Item();
+		item.setItemName(RandomStringUtils.randomAlphabetic(10));
+		item.getImages().add(RandomStringUtils.randomAlphabetic(5));
+		item.getImages().add(RandomStringUtils.randomAlphabetic(5));
+		item.getImages().add(RandomStringUtils.randomAlphabetic(5));
+		item.getImages().add(RandomStringUtils.randomAlphabetic(5));
+		entityManager.persist(item);
+		entityManager.flush();
+		logger.info("Before delete");
+		for(String image : item.getImages()){
+			logger.info("Image "+image);
+		}
+		item.getImages().remove(1);
+		/*This fires one delete query and two update queries.*/
+		entityManager.persist(item);
+		entityManager.flush();
+		logger.info("After delete");
+		for(String image : item.getImages()){
+			logger.info("Image "+image);
+		}
+	}
+	
+}
