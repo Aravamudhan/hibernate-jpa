19271d4b10b00681e108ca23304cf28df88e8158
Amudhan <aravamudhan.a@gmail.com>
2016-10-06 10:11:10 +0530

    composite keys

---
+++ src/main/java/com/amudhan/jpatest/model/complexschemas/compositekey/embedded/User.java
@@ -0,0 +1,11 @@
+package com.amudhan.jpatest.model.complexschemas.compositekey.embedded;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name = "COMPLEXSCHEMAS_COMPOSITEKEY_EMBEDED_USER")
+@Table(name = "COMPLEXSCHEMAS_COMPOSITEKEY_EMBEDED_USER")
+public class User {
+
+	
+}
--- src/test/java/com/amudhan/jpatest/model/complexschemas/naturalprimarykey/NaturalPrimaryKey.java
+++ src/test/java/com/amudhan/jpatest/model/complexschemas/naturalprimarykey/NaturalPrimaryKey.java
@@ -1,11 +1,15 @@
 package com.amudhan.jpatest.model.complexschemas.naturalprimarykey;
 
+import javax.persistence.EntityManager;
 import javax.transaction.UserTransaction;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertNotNull;
 
+import com.amudhan.jpatest.environment.DatabaseProduct;
 import com.amudhan.jpatest.environment.JPASetup;
 import com.amudhan.jpatest.environment.TransactionManagerTest;
 
@@ -14,8 +18,34 @@ public class NaturalPrimaryKey extends TransactionManagerTest{
 	private JPASetup jpa;
 	private Logger logger = LoggerFactory.getLogger(NaturalPrimaryKey.class);
 	
+	@BeforeClass
+	public void beforeMethod() {
+		jpa = new JPASetup(DatabaseProduct.H2, "NaturalPrimaryKeyPU");
+		jpa.dropSchema();
+		jpa.createSchema();
+	}
+	
 	@Test
 	public void storeLoad(){
 		UserTransaction tx = TRANSACTION_MANAGER.getUserTransaction();
+		try{
+			tx.begin();
+			EntityManager entityManager = jpa.createEntityManager();
+			User userOne = new User("Awesome user");
+			entityManager.persist(userOne);
+			tx.commit();
+			entityManager.close();
+			
+			tx.begin();
+			EntityManager entityManagerAnother = jpa.createEntityManager();
+			User userTwo = entityManagerAnother.find(User.class, "Awesome user");
+			assertNotNull(userTwo);
+			tx.commit();
+			entityManagerAnother.close();
+		}catch(Exception e){
+			logger.info(" Exception at storeLoad"+e);
+		}finally{
+			TRANSACTION_MANAGER.rollback();
+		}
 	}
 }
